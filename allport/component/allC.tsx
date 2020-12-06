c19de1b2f3dfc9eecfb88aeca6df71f66961884a
ahah12k <ahah12k@gmail.com>
2020-12-06 15:03:28 +0900

    erase treat data

--- allport/component/allC.tsx
+++ allport/component/allC.tsx
@@ -15,7 +15,7 @@ const AllMenu=()=>{
          <Link href="/lol"><a>LOL</a></Link>
          <Link href="/gikal"><a>귀칼</a></Link>
          <Link href="/svt"><a> 17 </a></Link>
-         <a target="_blank" rel="noopener noreferrer" href="http://data.honeyhyoni.shop/">Data</a>
+         {/*<a target="_blank" rel="noopener noreferrer" href="http://data.honeyhyoni.shop/">Data</a>*/}
          <Link href="/"><a>HOME</a></Link>
         </div>
     )
--- allport/exporthing/mytable.ts
+++ allport/exporthing/mytable.ts
@@ -7,5 +7,5 @@ export const myt=[
     {id:6,developing:false,explain:"CAF(React)",link:"/cafe",local:true},
     {id:7,developing:false,explain:"CAF(Spring)",link:"http://ahah12k.cafe24.com/jin",local:false},
     {id:8,developing:false,explain:"Seventeen",link:"/svt",local:true},
-    {id:9,developing:false,explain:"Treat Data",link:"http://data.honeyhyoni.shop/",local:false},
+    // {id:9,developing:false,explain:"Treat Data",link:"http://data.honeyhyoni.shop/",local:false},
 ]
